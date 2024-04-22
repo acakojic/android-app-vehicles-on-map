fa12ac109d03f02fb660b7da67b27308bf5838dc
acakojic <a.kojic@yahoo.com>
2024-04-22 19:00:22 +0200

    22.04.24 - MapViewModel Refactored

--- app/src/main/java/com/acakojic/zadataktcom/viewmodel/MapViewModel.kt
+++ app/src/main/java/com/acakojic/zadataktcom/viewmodel/MapViewModel.kt
@@ -63,6 +63,9 @@ import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 
 
+/**
+ * ViewModel to manage the map and vehicle-related operations.
+ */
 class MapViewModel(private val customRepository: CustomRepository, private val context: Context) :
     ViewModel() {
     private val _vehiclesByType = MutableLiveData<List<Vehicle>?>()
@@ -80,6 +83,9 @@ class MapViewModel(private val customRepository: CustomRepository, private val c
         fetchVehicles()
     }
 
+    /**
+     * Fetches vehicles based on the selected vehicle type.
+     */
     fun fetchVehicles() {
         viewModelScope.launch {
             val response = customRepository.getAllVehicles(context)
@@ -88,47 +94,64 @@ class MapViewModel(private val customRepository: CustomRepository, private val c
                     it.vehicleTypeID == selectedVehicleType.value.typeId
                 }
                 _allVehicles.value = response.body()!!
-
             } else {
+                Log.d("MapViewModel", "Failed to fetch vehicles")
                 _vehiclesByType.value = emptyList()
                 _allVehicles.value = emptyList()
             }
         }
     }
 
+    /**
+     * Sets the vehicle type and fetches vehicles accordingly.
+     */
     fun setVehicleType(type: VehicleType) {
         selectedVehicleType.value = type
         fetchVehicles()
         searchQuery.value = ""
+        Log.d("MapViewModel", "Vehicle type set and vehicles fetched for type: ${type.name}")
     }
 
 
+    /**
+     * Toggles the favorite status of a vehicle.
+     */
     fun toggleFavorite(vehicleId: Int, isFavorite: Boolean) {
         viewModelScope.launch {
 
             viewModelScope.launch {
                 val result = customRepository.addToFavorites(context, vehicleId)
                 if (result.isSuccess) {
+                    Log.d("MapViewModel", "Favorite status toggled for vehicle ID: $vehicleId")
                     _allVehicles.value = _allVehicles.value.map { vehicle ->
                         if (vehicle.vehicleID == vehicleId) vehicle.copy(isFavorite = isFavorite) else vehicle
                     }
                     _vehiclesByType.value = _vehiclesByType.value?.map { vehicle ->
                         if (vehicle.vehicleID == vehicleId) vehicle.copy(isFavorite = isFavorite) else vehicle
                     }
+                } else {
+                    Log.e("MapViewModel", "Failed to toggle favorite status for vehicle ID: $vehicleId")
                 }
             }
         }
     }
 
+    /**
+     * Updates the map markers based on the vehicle data.
+     */
     fun updateMapMarkers(
         mapView: MapView,
         vehicles: List<Vehicle>,
         onVehicleClick: (Vehicle) -> Unit
     ) {
         _vehiclesByType.value = vehicles
-        addMarkersToMap(context, mapView, vehicles, onVehicleClick)
+        createMarkerForVehicle(context, mapView, vehicles, onVehicleClick)
+        Log.d("MapViewModel", "Map markers updated")
     }
 
+    /**
+     * Responds to changes in search query by refetching vehicles.
+     */
     fun onSearchQueryChanged(query: String) {
         searchQuery.value = query
 
@@ -137,6 +160,93 @@ class MapViewModel(private val customRepository: CustomRepository, private val c
         } else {
         }
     }
+
+    /**
+     * Creates a marker for a vehicle on the map.
+     */
+    fun createMarkerForVehicle(
+        context: Context,
+        mapView: MapView,
+        vehicles: List<Vehicle>,
+        onVehicleClick: (Vehicle) -> Unit
+    ) {
+        mapView.overlays.clear()
+
+        vehicles.forEach { vehicle ->
+            val marker = Marker(mapView).apply {
+                position = GeoPoint(vehicle.location.latitude, vehicle.location.longitude)
+                title = vehicle.name
+                snippet = "${vehicle.price}€"
+                setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+                icon = getMarkerIconWithText(
+                    context = context,
+                    isFavorite = vehicle.isFavorite,
+                    price = vehicle.price
+                )
+
+                setOnMarkerClickListener { marker, mapView ->
+                    onVehicleClick(vehicle)
+                    true  //true to indicate that the event has been handled
+                }
+            }
+            mapView.overlays.add(marker)
+        }
+        mapView.invalidate()  //Refresh the map
+    }
+
+    fun getMarkerIconWithText(context: Context, isFavorite: Boolean, price: Double): Drawable {
+        val drawable = if (isFavorite) {
+            ContextCompat.getDrawable(context, R.drawable.circle_orange) //cars
+        } else {
+            ContextCompat.getDrawable(context, R.drawable.circle_black) // Default case
+        }
+
+        drawable?.let {
+            // bitmap from the drawable
+            val width = if (it.intrinsicWidth > 0) it.intrinsicWidth else 100
+            val height = if (it.intrinsicHeight > 0) it.intrinsicHeight else 100
+
+            val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(bitmap)
+            it.setBounds(0, 0, canvas.width, canvas.height)
+            it.draw(canvas)
+
+            // Draw text
+            val text = "$${price}"
+            val paint = Paint().apply {
+                color = android.graphics.Color.WHITE
+                textSize = 30f
+                textAlign = Paint.Align.CENTER
+            }
+            // Calculate vertical position to center text
+            val textHeight = paint.descent() - paint.ascent()
+            val textOffset = (textHeight / 2) - paint.descent()
+            canvas.drawText(text, canvas.width / 2f, canvas.height / 2f + textOffset, paint)
+
+            return BitmapDrawable(context.resources, bitmap)
+        }
+        return ContextCompat.getDrawable(context, R.drawable.circle_black)!! // Fallback
+    }
+}
+
+fun initializeMap(context: Context): MapView {
+    Configuration.getInstance()
+        .load(context, PreferenceManager.getDefaultSharedPreferences(context))
+
+    val mapView = MapView(context)
+    mapView.setTileSource(TileSourceFactory.MAPNIK)
+    mapView.setBuiltInZoomControls(true)
+//    mapView.isMultiTouchControls = true
+    mapView.controller.setZoom(12.0)
+    mapView.controller.setCenter(
+        //Belgrade starting location
+        GeoPoint(
+            44.81722374773659,
+            20.460807455759323
+        )
+    )
+
+    return mapView
 }
 
 @Composable
@@ -282,91 +392,6 @@ fun VehicleCard(
         }
         //end of this
     }
-}
-
-fun addMarkersToMap(
-    context: Context,
-    mapView: MapView,
-    vehicles: List<Vehicle>,
-    onVehicleClick: (Vehicle) -> Unit
-) {
-    mapView.overlays.clear()
-
-    vehicles.forEach { vehicle ->
-        val marker = Marker(mapView).apply {
-            position = GeoPoint(vehicle.location.latitude, vehicle.location.longitude)
-            title = vehicle.name
-            snippet = "${vehicle.price}€"
-            setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-            icon = getMarkerIconWithText(
-                context = context,
-                isFavorite = vehicle.isFavorite,
-                price = vehicle.price
-            )
-
-            setOnMarkerClickListener { marker, mapView ->
-                onVehicleClick(vehicle)
-                true  //true to indicate that the event has been handled
-            }
-        }
-        mapView.overlays.add(marker)
-    }
-    mapView.invalidate()  //Refresh the map
-}
-
-fun getMarkerIconWithText(context: Context, isFavorite: Boolean, price: Double): Drawable {
-    val drawable = if (isFavorite) {
-        ContextCompat.getDrawable(context, R.drawable.circle_orange) //cars
-    } else {
-        ContextCompat.getDrawable(context, R.drawable.circle_black) // Default case
-    }
-
-    drawable?.let {
-        // bitmap from the drawable
-        val width = if (it.intrinsicWidth > 0) it.intrinsicWidth else 100
-        val height = if (it.intrinsicHeight > 0) it.intrinsicHeight else 100
-
-        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(bitmap)
-        it.setBounds(0, 0, canvas.width, canvas.height)
-        it.draw(canvas)
-
-        // Draw text
-        val text = "$${price}"
-        val paint = Paint().apply {
-            color = android.graphics.Color.WHITE
-            textSize = 30f
-            textAlign = Paint.Align.CENTER
-        }
-        // Calculate vertical position to center text
-        val textHeight = paint.descent() - paint.ascent()
-        val textOffset = (textHeight / 2) - paint.descent()
-        canvas.drawText(text, canvas.width / 2f, canvas.height / 2f + textOffset, paint)
-
-        return BitmapDrawable(context.resources, bitmap)
-    }
-    return ContextCompat.getDrawable(context, R.drawable.circle_black)!! // Fallback
-}
-
-
-fun initializeMap(context: Context): MapView {
-    Configuration.getInstance()
-        .load(context, PreferenceManager.getDefaultSharedPreferences(context))
-
-    val mapView = MapView(context)
-    mapView.setTileSource(TileSourceFactory.MAPNIK)
-    mapView.setBuiltInZoomControls(true)
-//    mapView.isMultiTouchControls = true
-    mapView.controller.setZoom(12.0)
-    mapView.controller.setCenter(
-        //Belgrade starting location
-        GeoPoint(
-            44.81722374773659,
-            20.460807455759323
-        )
-    )
-
-    return mapView
 }
 
 
